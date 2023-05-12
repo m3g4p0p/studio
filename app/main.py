2fffa55a7196b0c846bb459f643ebcb3ec9cc306
Sebastian Seitz <bassistsagainstracists@gmx.net>
2023-05-12 23:40:25 +0200

    add deta base

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
+from datetime import date
+
+from deta import Deta
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
 app = FastAPI()
+deta = Deta()
+db = deta.Base('reservations')
+
+
+class Reservation(BaseModel):
+
+    band: str
+    date: date
 
 
 @app.get("/")
 def root():
     return "Hello from Space! 🚀"
+
+
+@app.post('/dates/')
+def create_reservation(item: Reservation):
+    db.insert(jsonable_encoder(item))
