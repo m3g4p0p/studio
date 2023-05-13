56cac57fd7a4ceabc6d3e9175a42f35baa9f2172
Sebastian Seitz <bassistsagainstracists@gmx.net>
2023-05-14 00:31:54 +0200

    add exception handler

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+
+from urllib.error import HTTPError
+
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import PlainTextResponse
 
 from .routers import api
 from .routers import web
@@ -7,3 +12,8 @@ app = FastAPI()
 
 app.mount('/', web.router)
 app.mount('/api', api.router)
+
+
+@app.exception_handler(HTTPError)
+def handle_http_error(request: Request, exc: HTTPError):
+    return PlainTextResponse(f'{exc.reason} 💀', exc.code)
