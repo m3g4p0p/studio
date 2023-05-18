4d22d00c208cbdeabfff26937f1fe100f6a41e10
Sebastian Seitz <bassistsagainstracists@gmx.net>
2023-05-19 01:40:08 +0200

    add route context

--- app/templating.py
+++ app/templating.py
@@ -10,7 +10,14 @@ def env_context(request: Request):
     return {'env': os.environ}
 
 
+def route_context(request: Request):
+    if 'route' not in request.scope:
+        return {'route': ''}
+
+    return {'route': request.scope['route'].name}
+
+
 templates = Jinja2Templates(
     directory=base_path / 'templates',
-    context_processors=[env_context],
+    context_processors=[env_context, route_context],
 )
