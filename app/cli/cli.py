3e2dd23f312622110dac54a27ea1e9ba07dc6a44
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-10-06 20:33:19 +0200

    add reservation model

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -2,6 +2,20 @@ import json
 from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 from argparse import FileType
+from datetime import date as pydate
+
+from pydantic import BaseModel
+from pydantic import TypeAdapter
+from pydantic import ValidationError
+
+
+class Reservation(BaseModel):
+
+    band: str
+    date: pydate
+
+
+ReservationList = TypeAdapter(list[Reservation])
 
 
 class JSONType(FileType):
@@ -10,9 +24,14 @@ class JSONType(FileType):
         file = super().__call__(string)
 
         try:
-            return json.load(file)
+            data = json.load(file)
         except json.JSONDecodeError as e:
             raise ArgumentTypeError(*e.args)
+
+        try:
+            return ReservationList.validate_python(data)
+        except ValidationError as e:
+            raise ArgumentTypeError(e)
 
 
 parser = ArgumentParser()
