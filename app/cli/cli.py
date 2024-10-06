738d655de1fd0af8c9654d30e24e9a0faae2a2c5
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-10-06 20:22:31 +0200

    add argument parser

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,2 +1,25 @@
+import json
+from argparse import ArgumentParser
+from argparse import ArgumentTypeError
+from argparse import FileType
+
+
+class JSONType(FileType):
+
+    def __call__(self, string: str):
+        file = super().__call__(string)
+
+        try:
+            return json.load(file)
+        except json.JSONDecodeError as e:
+            raise ArgumentTypeError(*e.args)
+
+
+parser = ArgumentParser()
+parser.add_argument('--json', type=JSONType())
+parser.add_argument('--drop', action='store_true')
+
+
 async def main():
-    pass
+    args_ = parser.parse_args()
+    return args_
