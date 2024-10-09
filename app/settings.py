444cfe5689fd06c62797b6dec03da486fd0dacb6
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-10-09 08:36:10 +0200

    add basic auth settings

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,22 @@
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
+
+
+class BasicAuthSettings(BaseSettings):
+
+    username: str
+    password: SecretStr
 
 
 class AppSettings(BaseSettings):
 
     sqlalchemy_database_url: str
+    basic_auth: BasicAuthSettings
 
     model_config = SettingsConfigDict(
+        env_nested_delimiter='__',
+        env_file_encoding='utf-8',
         env_file='.env',
         extra='ignore',
     )
