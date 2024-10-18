6620645795d8d9d5cb41eafefd00ef5b1d4efcec
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-10-18 16:23:05 +0200

    add subcommand

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,13 +1,14 @@
 from argparse import ArgumentParser
-from argparse import ArgumentTypeError
-from argparse import FileType
 from datetime import date as pydate
 from typing import Optional
 
 from pydantic import BaseModel
-from pydantic import ConfigDict
+from pydantic import FilePath
 from pydantic import TypeAdapter
-from pydantic import ValidationError
+from pydantic_settings import BaseSettings
+from pydantic_settings import CliSettingsSource
+from pydantic_settings import CliSubCommand
+from pydantic_settings import get_subcommand
 from rich.console import Console
 
 from app.database import async_session
@@ -22,53 +23,66 @@ class ReservationImport(BaseModel):
     date: pydate
 
 
-class ParserModel(BaseModel):
+class BaseCommand(BaseModel):
 
-    drop: bool
-    json: Optional[list[ReservationImport]]
-    model_config = ConfigDict(from_attributes=True)
-
-
-class JSONType(FileType):
-
-    adapter = TypeAdapter(list[ReservationImport])
-
-    def __call__(self, string: str):
-        file = super().__call__(string)
-
-        try:
-            return self.adapter.validate_json(file.read())
-        except ValidationError as e:
-            raise ArgumentTypeError(e)
+    async def invoke(self):
+        raise NotImplementedError
 
 
+class CreateCommand(BaseCommand):
+
+    drop: bool = False
+    jsonfile: Optional[FilePath] = None
+
+    async def invoke(self):
+        async with engine.begin() as conn:
+            if self.drop:
+                await conn.run_sync(Base.metadata.drop_all)
+
+            await conn.run_sync(Base.metadata.create_all)
+
+        if not self.jsonfile:
+            return
+
+        adapter = TypeAdapter(list[ReservationImport])
+
+        with open(self.jsonfile) as f:
+            data = adapter.validate_json(f.read())
+
+        async with async_session() as session:
+            for model in data:
+                reservation = Reservation(
+                    date=model.date,
+                    band=model.band,
+                )
+
+                session.add(reservation)
+                console.print(f'{reservation.date} {reservation.band}')
+
+            await session.commit()
+
+
+class CliSettings(BaseSettings):
+
+    create: CliSubCommand[CreateCommand]
+
+
+console = Console(highlight=True)
 parser = ArgumentParser()
-parser.add_argument('--json', type=JSONType())
-parser.add_argument('--drop', action='store_true')
+
+source = CliSettingsSource(
+    CliSettings,
+    root_parser=parser,
+    cli_implicit_flags=True,
+)
 
 
 async def main():
-    args = ParserModel.model_validate(
-        parser.parse_args())
-    console = Console(highlight=True)
+    args = parser.parse_args()
 
-    async with engine.begin() as conn:
-        if args.drop:
-            await conn.run_sync(Base.metadata.drop_all)
+    model = source.settings_cls(
+        _cli_settings_source=source(parsed_args=args),
+    )
 
-        await conn.run_sync(Base.metadata.create_all)
-
-    if not args.json:
-        return
-
-    async with async_session() as session:
-        for model in args.json:
-            reservation = Reservation(
-                date=model.date,
-                band=model.band,
-            )
-
-            session.add(reservation)
-            console.print(f'{reservation.date} {reservation.band}')
-
-        await session.commit()
+    command: BaseCommand = get_subcommand(model)
+    await command.invoke()
