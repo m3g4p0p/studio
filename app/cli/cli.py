102d7cf458b7be197ce42b8ece265dd3a11b5925
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-10-18 16:43:50 +0200

    add import data field

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,10 +1,13 @@
+import json
 from argparse import ArgumentParser
 from datetime import date as pydate
 from typing import Optional
 
 from pydantic import BaseModel
+from pydantic import Field
 from pydantic import FilePath
-from pydantic import TypeAdapter
+from pydantic import ValidationInfo
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
 from pydantic_settings import CliSettingsSource
 from pydantic_settings import CliSubCommand
@@ -34,6 +37,20 @@ class CreateCommand(BaseCommand):
     drop: bool = False
     jsonfile: Optional[FilePath] = None
 
+    import_data: Optional[list[ReservationImport]] = Field(
+        init=False, default=None, validate_default=True,
+    )
+
+    @field_validator('import_data', mode='before')
+    def read_import_data(cls, _, info: ValidationInfo):
+        file_path = info.data['jsonfile']
+
+        if file_path is None:
+            return None
+
+        with open(file_path) as f:
+            return json.load(f)
+
     async def invoke(self):
         async with engine.begin() as conn:
             if self.drop:
@@ -41,16 +58,11 @@ class CreateCommand(BaseCommand):
 
             await conn.run_sync(Base.metadata.create_all)
 
-        if not self.jsonfile:
+        if not self.import_data:
             return
 
-        adapter = TypeAdapter(list[ReservationImport])
-
-        with open(self.jsonfile) as f:
-            data = adapter.validate_json(f.read())
-
         async with async_session() as session:
-            for model in data:
+            for model in self.import_data:
                 reservation = Reservation(
                     date=model.date,
                     band=model.band,
